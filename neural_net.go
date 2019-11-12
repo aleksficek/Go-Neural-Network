1b23f6d1ba9d8baaac9c567af139d23ade7b3d37
Aleksander Ficek <aleksanderficek@Administrators-MacBook-Pro.local>
2019-11-13 00:08:29 +0800

    Added sigmoid function

--- neural_net.go
+++ neural_net.go
@@ -1,1 +1,13 @@
+package net
+
+import "math"
+
 // This is the start of the neural net in golang
+
+type GoNetwork struct {
+	input int
+}
+
+func sigmoid(input float64) float64 {
+	return 1.0 / (1 + math.Exp(-1*input))
+}
