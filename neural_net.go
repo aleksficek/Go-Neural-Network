68818418ea7b5f60618b96226ecee93d620616af
aleksficek <alex.ficek99@gmail.com>
2019-12-08 22:10:44 +0900

    Added more helper funcitons

--- neural_net.go
+++ neural_net.go
@@ -8,7 +8,7 @@ import (
 	"gonum.org/v1/gonum/mat"
 )
 
-// This is the start of the neural net in golang
+// This is the start of the neural net in golang!
 
 // Node is a data structure that represents a neural network node
 type Node struct {
@@ -18,40 +18,81 @@ type Node struct {
 // GoNetwork is the neural network struct
 type GoNetwork struct {
 	speedOfNetwork float64
-	inputNodes     []Node
-	midNodes       []Node
-	outputNodes    []Node
+	inputs         int
+	mids           int
+	outputs        int
 	firstWeights   *mat.Dense
 	secondWeights  *mat.Dense
 }
 
 // MakeGoNetwork creates an instance of the neural network
-func MakeGoNetwork(inputs, mids, outputs []Node, speedOfNetwork float64) {
-	randomWeights1 := 1
+func MakeGoNetwork(inputs, mids, outputs int, speedOfNetwork float64) {
 
-	// neuralNet := GoNetwork{
-	// 	speedOfNetwork: speedOfNetwork,
-	// 	inputNodes:     inputs,
-	// 	midNodes:       mids,
-	// 	outputNodes:    outputs,
-	// 	firstWeights: 	mat.NewDense(mids, inputs, [])
-	// 	secondWeights:	mat.newDense(outputs, mids, [])
-	// }
-	// fmt.Print(neuralNet)
+	neuralNet := GoNetwork{
+		speedOfNetwork: speedOfNetwork,
+		inputs:         inputs,
+		mids:           mids,
+		outputs:        outputs,
+		firstWeights:   mat.NewDense(mids, inputs, createRandomArray(mids)),
+		secondWeights:  mat.NewDense(outputs, mids, createRandomArray(outputs)),
+	}
+	fmt.Print(neuralNet)
+}
+
+// TrainOnce performs a single iteration of applying weights and activating function to network
+func (n *GoNetwork) TrainOnce(data []float64) mat.Matrix {
+	newInputs := mat.NewDense(n.inputs, 1, data)
+	newMids := dotProduct(n.firstWeights, newInputs)
+	newMidsActivated := activateMatrix(sigmoid, newMids)
+	newOuts := dotProduct(n.secondWeights, newMidsActivated)
+	return activateMatrix(sigmoid, newOuts)
 }
 
 func main() {
 	fmt.Print("Welcome to the golang neural network!")
+	myNet := &GoNetwork{
+		speedOfNetwork: 13,
+		inputs:         17,
+		mids:           15,
+		outputs:        3,
+	}
 
+	data := [...]float64{13, 14, 15, 13, 17, 2, 4, 5, 7}
+	firstPrediction := myNet.TrainOnce(data)
 }
 
-func createRandomArray(n int) []int {
-	array := make([]int, n)
-	rand.Read(array)
-	fmt.Println("Creating a random array: %v", array)
+// createRandomArray creates a new array of size n, full with random values
+func createRandomArray(n int) []float64 {
+	fmt.Println("Creating a random array!")
+	array := make([]float64, n)
+	for i := range array {
+		if i%2 == 0 {
+			array[i] = rand.Float64()
+		} else {
+			array[i] = (-1) * rand.Float64()
+		}
+	}
+	return array
+}
+
+// activateMatrix applies an activationFunction to a specified matrix
+func activateMatrix(activationFunction func(x, y int, input float64) float64, matrix mat.Matrix) mat.Matrix {
+	x, y := matrix.Dims()
+	resultMatrix := mat.NewDense(x, y, nil)
+	resultMatrix.Apply(activationFunction, matrix)
+	return resultMatrix
+}
+
+// dotProduct takes the dot product of two matrices
+func dotProduct(matrixA, matrixB mat.Matrix) mat.Matrix {
+	x, _ := matrixA.Dims()
+	_, y := matrixB.Dims()
+	resultMatrix := mat.NewDense(x, y, nil)
+	resultMatrix.Product(matrixA, matrixB)
+	return resultMatrix
 }
 
 // sigmoid takes an input and returns the value post sigmoid calculation
-func sigmoid(input float64) float64 {
+func sigmoid(x, y int, input float64) float64 {
 	return 1.0 / (1 + math.Exp(-1*input))
 }
