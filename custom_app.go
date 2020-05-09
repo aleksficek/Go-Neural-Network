3b43aea2d0f07f81d00394ec93fb5c40ec6cac61
aleksficek <alex.ficek99@gmail.com>
2020-05-08 22:06:12 -0400

    Dataset cleaning

--- custom_app.go
+++ custom_app.go
@@ -2,21 +2,23 @@ package main // This will need to change and become abstracted
 
 import (
 	"fmt"
-	"math"
-	"math/rand"
+	// "math"
+	// "math/rand"
 	"os"
-	"io"
-	"strconv"
-	"log"
-	"gonum.org/v1/gonum/mat"
-	"encoding/csv"
-	"bufio"
-	"image/png"
-	"image"
+	// "io"
+	// "strconv"
+	// "log"
+	// "gonum.org/v1/gonum/mat"
+	// "encoding/csv"
+	// "bufio"
+	// "image/png"
+	// "image"
 )
 
 func parseData(path string) {
 	// open file
+	checkFile, _ := os.Open("covid19/dataset_clean_2.csv")
+	defer checkFile.Close() 
 	
 }
 
@@ -26,5 +28,7 @@ func main() {
 	// Create the neural net
 	numbersNet := MakeGoNetwork(784, 200, 10, 1)
 	// print(numbersNet)
+
+	
 }
 
